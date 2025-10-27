a73d633aaf346a7de335d59725e5a38e93c0bbed
jules-pecaoco <julesalfonzp@gmail.com>
2025-10-27 11:19:50 +0800

    updated apk

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ export default function TanawLandingPage() {
   // App version
   const appVersion = "2.5.0";
   const appCode = "Tanaw-dev.apk";
-  const appLink = `https://github.com/jules-pecaoco/tanaw-v2/releases/download/vdev.16/Tanaw-dev.apk`;
+  const appLink = `https://release-assets.githubusercontent.com/github-production-release-asset/1011305656/5aa4e8fc-4c00-49fb-bda7-e64d1341903c?sp=r&sv=2018-11-09&sr=b&spr=https&se=2025-10-27T04%3A05%3A58Z&rscd=attachment%3B+filename%3DTanaw-dev.apk&rsct=application%2Fvnd.android.package-archive&skoid=96c2d410-5711-43a1-aedd-ab1947aa7ab0&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skt=2025-10-27T03%3A04%3A59Z&ske=2025-10-27T04%3A05%3A58Z&sks=b&skv=2018-11-09&sig=iHHxhDh5XiETNH7bAynusnv%2B94rIOSaX93ntrp7OTQc%3D&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmVsZWFzZS1hc3NldHMuZ2l0aHVidXNlcmNvbnRlbnQuY29tIiwia2V5Ijoia2V5MSIsImV4cCI6MTc2MTUzNzg5OSwibmJmIjoxNzYxNTM0Mjk5LCJwYXRoIjoicmVsZWFzZWFzc2V0cHJvZHVjdGlvbi5ibG9iLmNvcmUud2luZG93cy5uZXQifQ.9vjuzKaD-ONjqidln6MWBOo-sZQHHKCuDOUeDA5UXls&response-content-disposition=attachment%3B%20filename%3DTanaw-dev.apk&response-content-type=application%2Fvnd.android.package-archive`;
 
   useEffect(() => {
     const handleScroll = () => {
